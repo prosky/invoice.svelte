c022c803129ea5b2f4622685a5e43fd23190a3f5
Jan Proskovec <proskovec.jan@gmail.com>
2020-11-24 05:31:54 +0100

    nothing

---
+++ src/components/Inputs/ImagePicker.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+    export let format = (val) => items[val];
+    export let value: any;
+
+</script>
+
+
+<div class="image-picker">
+    <input type="file">
+    <slot/>
+</div>
