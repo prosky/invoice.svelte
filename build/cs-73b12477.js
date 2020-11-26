5512f38513da192e29cc74398147a1a8148d5d16
Jan Proskovec <jan.proskovec@nativo.cz>
2020-11-26 11:31:37 +0100

    Updates

--- build/cs-73b12477.js
+++
@@ -1,2 +0,0 @@
-export default{page:{home:{title:"Homepage",nav:"Home"},about:{title:"About",nav:"About"},contact:{title:"Contact",nav:"Contact Us"},settings:{title:"Settings",nav:"Settings"}}};
-//# sourceMappingURL=cs-73b12477.js.map
--- build/en-2ac07913.js
+++
@@ -1,2 +0,0 @@
-export default{page:{home:{title:"Homepage",nav:"Home"},about:{title:"About",nav:"About"},contact:{title:"Contact",nav:"Contact Us"},settings:{title:"Settings",nav:"Settings"}}};
-//# sourceMappingURL=en-2ac07913.js.map
